245922313ea3f0da77791c289793f283e6eaa3a2
Sarinezs <a.edr.edm.ed@gmail.com>
2023-07-14 20:21:43 +0700

    java(success)

---
+++ test.java
@@ -0,0 +1,6 @@
+public class test {
+    public static void main(String[] args) throws Exception{
+        System.out.println("hello world! arm");
+        System.out.println("suriya meerua");
+    }
+}

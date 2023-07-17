6e443b994509f200fb5191191e53029c87ff7589
Sarinezs <a.edr.edm.ed@gmail.com>
2023-07-17 21:14:59 +0700

    .

--- test.java
+++
@@ -1,6 +0,0 @@
-public class test {
-    public static void main(String[] args) throws Exception{
-        System.out.println("hello world! arm");
-        System.out.println("suriya meerua");
-    }
-}
---
+++ week1/ex1.java
@@ -0,0 +1,14 @@
+// package week1;
+
+import java.util.Scanner;
+
+public class ex1 {
+    public static void main(String[] args) {
+        // System.out.println("Suriya meerua");
+        double a;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter value: ");
+        a = sc.nextDouble();
+        System.out.println("your Entered value: "+a);    
+    }    
+}
